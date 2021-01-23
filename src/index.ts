2de34b8ed741413352369c9049dd7b1fcb3093ff
Marketa Solanikova <solanikova@promark.sk>
2021-01-23 22:17:57 +0100

    Add logs and proper error handling

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import SecretsManager from 'aws-sdk/clients/secretsmanager';
 import axios from 'axios';
 import { FeedlyAuth } from './FeedlyAuth';
 
+const REQUEST_LIMIT = 15;
+
 export const lambdaHandler = async (event: ScheduledEvent): Promise<void> => {
   const feedlyAuth = await getFeedlyToken();
 
@@ -10,7 +12,7 @@ export const lambdaHandler = async (event: ScheduledEvent): Promise<void> => {
 
   const duplicatedArticleIds = await getDuplicatedArticleIds(feedlyAuth);
 
-  markDuplicatedArticlesAsRead(feedlyAuth, duplicatedArticleIds);
+  await markDuplicatedArticlesAsRead(feedlyAuth, duplicatedArticleIds);
 };
 
 const getDuplicatedArticleIds = async (feedlyAuth: FeedlyAuth): Promise<string[]> => {
@@ -22,20 +24,27 @@ const getDuplicatedArticleIds = async (feedlyAuth: FeedlyAuth): Promise<string[]
   let continuation = '';
   let allProcessed = false;
 
-  while (!allProcessed) {
-    unreadArticlesResponse = await axios.get(`/v3/streams/contents`, {
-      params: {
-        streamId: `user/${feedlyAuth.user}/category/global.all`,
-        unreadOnly: 'true',
-        continuation: continuation,
-      },
-      headers: {
-        Authorization: `OAuth ${feedlyAuth.token}`,
-      },
-    });
+  let requestCount = 0;
 
-    if (unreadArticlesResponse.status != 200) {
-      throw new Error(`Couldn't retrieve unread articles. Response: ${JSON.stringify(unreadArticlesResponse)}`);
+  while (!allProcessed || requestCount < REQUEST_LIMIT) {
+    console.debug(`Request number: ${requestCount}`);
+    try {
+      unreadArticlesResponse = await axios.get(`/v3/streams/contents`, {
+        params: {
+          streamId: `user/${feedlyAuth.user}/category/global.all`,
+          unreadOnly: 'true',
+          continuation: continuation,
+        },
+        headers: {
+          Authorization: `OAuth ${feedlyAuth.token}`,
+        },
+      });
+      requestCount++;
+    } catch (err) {
+      console.error(`Couldn't retrieve unread articles. Response status: ${err.response.status}`);
+      // todo: check if token expired (401) - send sns message
+      // todo: check if rate limit exceeded (429) - send sns message
+      throw err;
     }
 
     for (const item of unreadArticlesResponse.data.items) {
@@ -49,29 +58,36 @@ const getDuplicatedArticleIds = async (feedlyAuth: FeedlyAuth): Promise<string[]
     continuation = unreadArticlesResponse.data.continuation;
 
     if (!continuation) {
-      console.debug(`Finished retrieving all unread articles`);
+      console.debug(`Finished retrieving all unread articles with ${requestCount} requests`);
       allProcessed = true;
     }
   }
 
+  console.debug(`Found ${duplicatedArticlesIds.length} duplicated articles`);
   return duplicatedArticlesIds;
 };
 
 const markDuplicatedArticlesAsRead = async (feedlyAuth: FeedlyAuth, duplicatedArticleIds: string[]) => {
+  if (duplicatedArticleIds.length == 0) {
+    return;
+  }
+
   const requestBody = {
     action: 'markAsRead',
     type: 'entries',
     entryIds: duplicatedArticleIds,
   };
 
-  const markerResponse = await axios.post(`/v3/markers`, requestBody, {
-    headers: {
-      Authorization: `OAuth ${feedlyAuth.token}`,
-    },
-  });
-
-  if (markerResponse.status != 200) {
-    throw new Error(`Couldn't mark articles as unread. Response: ${JSON.stringify(markerResponse)}`);
+  try {
+    await axios.post(`/v3/markers`, requestBody, {
+      headers: {
+        Authorization: `OAuth ${feedlyAuth.token}`,
+      },
+    });
+    console.debug(`All ${duplicatedArticleIds.length} duplicated articles were marked as read`);
+  } catch (err) {
+    console.error(`Couldn't mark articles as unread. Response status: ${err.response.status}`);
+    throw err;
   }
 };
 
